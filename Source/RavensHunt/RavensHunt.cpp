ab86cf521f56461dcc4b90fb8ef68e7bce18caee
slylii <102788866+slylii@users.noreply.github.com>
2026-06-21 13:11:06 +0300

    basic project

---
+++ Source/RavensHunt/RavensHunt.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RavensHunt.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RavensHunt, "RavensHunt" );
+
+DEFINE_LOG_CATEGORY(LogRavensHunt)
---
+++ Source/RavensHunt/RavensHuntGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RavensHuntGameMode.h"
+
+ARavensHuntGameMode::ARavensHuntGameMode()
+{
+	// stub
+}
